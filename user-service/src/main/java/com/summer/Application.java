f65477fcef2500d5512e31903d2ff2543b744db9
Summer <su943515688@gmail.com>
2019-06-25 23:51:09 +0800

    [+] 通过eureka管理微服务

--- user-service/src/main/java/com/summer/Application.java
+++ user-service/src/main/java/com/summer/Application.java
@@ -2,10 +2,12 @@ package com.summer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.summer.mapper")
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
