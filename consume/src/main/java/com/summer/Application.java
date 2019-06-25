3b2a4e05ecf5542eb94e1dc22a9a3c0e8b90c101
Summer <su943515688@gmail.com>
2019-06-26 00:37:37 +0800

    [+] 使用ribbon做负载均衡

--- consume/src/main/java/com/summer/Application.java
+++ consume/src/main/java/com/summer/Application.java
@@ -3,6 +3,7 @@ package com.summer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class Application {
 
     @Bean
+    @LoadBalanced // 开启负载均衡
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- consume/src/main/java/com/summer/web/Controller/UserController.java
+++ consume/src/main/java/com/summer/web/Controller/UserController.java
@@ -2,11 +2,15 @@ package com.summer.web.Controller;
 
 import com.summer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("consume")
@@ -15,9 +19,27 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{id}")
-    public User getUser(@PathVariable("id") Long id) {
-        String url = "http://localhost:8101/user/" + id;
+    public User getUser0(@PathVariable("id") Long id) {
+        // 从eureka获取可用服务
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("user-service");
+        ServiceInstance instance = serviceInstances.get(0);
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
         return this.restTemplate.getForObject(url, User.class);
+    }
+
+    /**
+     * 使用ribbon做负载均衡
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/loadbance/{id}")
+    public User getUser1(@PathVariable("id") Long id) {
+        String baseUrl = "http://user-service/user/" + id;
+        return restTemplate.getForObject(baseUrl, User.class);
     }
 }
